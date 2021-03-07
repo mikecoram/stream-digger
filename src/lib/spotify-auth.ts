42779b001eee7a658d1915a20dcec1e2e0f3e541
mikecoram <mikecoram96@gmail.com>
2021-03-07 18:06:16 +0000

    use localhost redirect url

--- src/lib/spotify-auth.ts
+++ src/lib/spotify-auth.ts
@@ -1,7 +1,7 @@
 export const getImplicitGrantURI = (): string =>
   `https://accounts.spotify.com/authorize?${new URLSearchParams({
     client_id: 'd48e33355f30490aa2a952bbf70055ad',
-    redirect_uri: 'https://mikecoram.github.io/buy-music',
+    redirect_uri: 'http://localhost:3000',
     response_type: 'token',
     state: Math.random().toString()
   }).toString()}`
