d0298d2708f71522802c919f3f83c24ff0124cf3
mikecoram <mikecoram96@gmail.com>
2021-03-28 19:50:48 +0100

    login css

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { getImplicitGrantURI } from './lib/spotify-auth'
+import './Login.css'
 
 export class Login extends React.Component {
   render (): React.ReactElement {
     return (
       <a
         id='login-with-spotify'
+        className="loginWithSpotify"
         href={getImplicitGrantURI()}
       >
-        Login with Spotify...
+        Login with Spotify
       </a>
     )
   }
