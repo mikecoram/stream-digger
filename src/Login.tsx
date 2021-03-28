6962533ff2a6fd67aa3849d6469430815bafa0c1
mikecoram <mikecoram96@gmail.com>
2021-03-28 16:13:17 +0100

    standard

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react'
 import { getImplicitGrantURI } from './lib/spotify-auth'
 
 export class Login extends React.Component {
-  render() {
-    return <a
-    id='login-with-spotify'
-    href={getImplicitGrantURI()}
-  >
-    Login with Spotify...
-  </a>
+  render (): React.ReactElement {
+    return (
+      <a
+        id='login-with-spotify'
+        href={getImplicitGrantURI()}
+      >
+        Login with Spotify...
+      </a>
+    )
   }
 }
