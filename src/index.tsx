81a0af10642d6eb5da66bfda9aff5ff6bc1ca56c
mikecoram <mikecoram96@gmail.com>
2021-04-07 10:33:21 +0100

    refactor oauth callback

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import App from './components/App'
 import { LocalStorageOAuthSession } from './lib/local-storage-oauth-session'
 import { LocalStorageSpotifyOAuth } from './lib/local-storage-spotify-oauth'
 
-const urlParams = new URLSearchParams(window.location.search)
-const state = urlParams.get('state')
-const code = urlParams.get('code')
+const oauthCallbackParams = new URLSearchParams(window.location.search)
 
-if (code !== null && state !== null) {
-  const error = urlParams.get('error')
+if (oauthCallbackParams.get('state') !== null) {
+  const error = oauthCallbackParams.get('error')
 
   if (error !== null) {
     throw new Error(error)
   }
 
   const oauth = new LocalStorageSpotifyOAuth()
+  const state = oauthCallbackParams.getAll('state')[0]
+  const code = oauthCallbackParams.getAll('code')[0]
 
   if (state === oauth.getLocalState()) {
     oauth.getToken(code)
